04f9731d568eed1e1e35d3c620c7a3120e99f439
Ignacio Mella <ignacio@acid.cl>
2013-01-31 19:39:25 -0300

    Added application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SignificantWeb::Application.config.secret_token = '66c31813e8eeb19cee89d09ab9c40a08ba0dbe234b334309aa04cb0103ca902435090cfb1d62b6f975072eb3ac9bfd0106c18966b7b0fdcf9129e76288b45817'
