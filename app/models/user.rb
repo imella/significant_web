1b4b199b0813b6e46f9d7788034f45292dd508a1
Ignacio Mella <ignacio@acid.cl>
2013-02-02 14:52:09 -0300

    Added oauth_expires_at attribute to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :birthday, :email, :name, :oauth_token, :provider, :uid
 
-  validates :name, :email, :oauth_token, :provider, :uid, presence: true
+  validates :name, :email, :oauth_token, :uid, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130202171416) do
+ActiveRecord::Schema.define(:version => 20130202174334) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(:version => 20130202171416) do
     t.string   "oauth_token"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "gender"
+    t.string   "localtion"
   end
 
 end
