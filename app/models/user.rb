b858a36c9045075e6b4dbce7d868086396e06982
Ignacio Mella <ignacio@acid.cl>
2013-02-02 16:53:43 -0300

    Includo root in json response

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :birthday, :email, :name, :oauth_token, :provider, :uid
+  attr_accessible :birthday, :email, :name, :oauth_token, :uid, :oauth_expires_at
 
   validates :name, :email, :oauth_token, :uid, presence: true
 
