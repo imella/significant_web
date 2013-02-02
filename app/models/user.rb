53af53774f343d38e537a4d39ab4e65d36874a0c
Ignacio Mella <ignacio@acid.cl>
2013-02-02 14:44:42 -0300

    Added some user endpoints and more attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :birthday, :email, :name, :oauth_token, :provider, :uid
+
+  validates :name, :email, :oauth_token, :provider, :uid, presence: true
+
 end
