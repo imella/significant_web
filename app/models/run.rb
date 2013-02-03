e922a14c9613cde4b78d3ec840c5d01ff8586f93
Ignacio Mella <ignacio@acid.cl>
2013-02-03 18:42:35 -0300

    Added run validations

--- app/models/run.rb
+++ app/models/run.rb
@@ -4,6 +4,7 @@ class Run < ActiveRecord::Base
   belongs_to :user
   attr_accessible :average_speed, :ended_at, :started_at, :total_distance, :total_time, :run_type_id, :user_id, :race_id
 
+  validates :average_speed, :started_at, :ended_at, :total_distance, :total_time, :run_type_id, :user_id, :race_id, presence: true
 
   scope :by_type, lambda { |run_type_id| where(run_type_id: run_type_id)}
 
